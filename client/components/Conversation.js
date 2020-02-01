404ebc2de5329bd3e10acaae061a54e947c122ce
Keshav <km02@bu.edu>
2020-02-01 13:47:01 -0500

    added conversation.js

---
+++ client/components/Conversation.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { GiftedChat } from "react-native-gifted-chat";
+
+export default class Conversation extends React.Component {
+
+    render() {
+        return (
+            <GiftedChat/>
+        )
+    }
+}
