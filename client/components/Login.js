33df5110f017ebbe3d44d6ba15060b1747873c2a
Keshav <km02@bu.edu>
2020-02-01 12:29:36 -0500

    login component

---
+++ client/components/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {View, Text} from "react-native";
+
+export default class Login extends React.Component {
+
+    render() {
+        return(
+            <View>
+                <Text> Welcome to WhatsAppLite </Text>
+            </View>
+        )
+    }
+
+}
